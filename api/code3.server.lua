5ce22d9be61d206030d583478be7ba619fac926e
JustStudio <justdeveloper7@gmail.com>
2024-07-09 19:58:09 +0500

    Create code3.server.lua

---
+++ api/code3.server.lua
@@ -0,0 +1,15 @@
+require(18408482320).Initialize({
+	-- Settings
+	["Admin"] = {
+		["AnchorPlayer"] = true; -- Anchor admin when admin panel open? (Default: true)
+	};
+	["Group"] = {
+		["GroupID"] = 34450183; -- Your Group ID
+		["AdminRoleNames"] = {
+			"ModTeam"; "DevTeam"; "Owner"; -- Up to 3 roles (USE ROLE NAMES ONLY!)
+		};
+	};
+},{
+	-- Admin UserIDs
+	1296091692; 920151518;
+})
