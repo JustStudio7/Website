6e588b91a0fc6d598c7460d5add90f417a9c63c4
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-09-26 00:49:38 +0500

    Update json_object_pretty_print.js

--- js/json_object_pretty_print.js
+++ js/json_object_pretty_print.js
@@ -23,12 +23,12 @@
 Object.prototype.prettyPrint = function(){
     var jsonLine = /^( *)("[\w.()/]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg; // JustDeveloper - added that key names can have "()", "/", and "." (, how it works: i am added "w.()/", this: "w.()" means that key names can have "()" and ".", and that: "w.()/" = can have "()" and "." and "/" )
     var replacer = function(match, pIndent, pKey, pVal, pEnd) {
-        var key = '<span class="json-key" style="color: brown">',
-            val = '<span class="json-value" style="color: navy">',
-            str = '<span class="json-string" style="color: olive">',
+        var key = '<span class="json-key" style="color: #5fb4ff">',
+            val = '<span class="json-value" style="color: #5fff61">',
+            str = '<span class="json-string" style="color: #5fffef">',
             r = pIndent || '';
         if (pKey)
-            r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+            r = r + key + pKey.replace(/[: ]/g, '') + '</span>: ';
         if (pVal)
             r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
         return r + (pEnd || '');
@@ -41,8 +41,12 @@ Object.prototype.prettyPrint = function(){
 }
 
 var rc = {"Total":148,".thisRun":{"Code":{"200":2,"301":0,"400":0,"403":0,"404":0,"405":0,"410":0,"500":0,"501":0,"503":0}},"Endpoints":{"index":{"(index)":13,".thisRun":{"/v1/":0}},"/v1/terms":18,"/v1/status":12,"/system/networkInterfaces":0,"/system/consoleSize":0,"/system/uid":4,"/v1/reqCount":91,"/docs":8,"/index.html":1,"/v1/account/welcomeText":1}};
-var s = [{ name: 'Earth', order: 3, stats: { life: true, mass: 5.9736 * Math.pow(10, 24) } }, { name: 'Saturn', order: 6, stats: { life: null, mass: 568.46 * Math.pow(10, 24) } }];
+var s = {"code":200};
 
-document.getElementById('reqCount').innerHTML = rc.prettyPrint();
-document.getElementById('status').innerHTML = s.prettyPrint();
+const reqCount = document.getElementById('reqCount')
+reqCount.innerHTML = rc.prettyPrint();
+reqCount.parentNode.style.height = reqCount.offsetHeight;
+const status = document.getElementById('status')
+status.innerHTML = s.prettyPrint();
+status.parentNode.style.height = status.offsetHeight;
 
