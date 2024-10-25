5f8306932c0591655d9c9dd39276446fba39d56b
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-10-26 00:49:40 +0500

    Update acc-bg.js

--- js/acc-bg.js
+++ js/acc-bg.js
@@ -10,6 +10,8 @@
 
 
 
+var canvas = document.getElementById('container');
+var ctx = canvas.getContext('2d');
 const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
 var w = 0;
