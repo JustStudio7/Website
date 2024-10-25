235d25ec17256166eef51ffcafad0e7f14d86604
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-10-26 00:32:48 +0500

    Update acc-bg.js

--- js/acc-bg.js
+++ js/acc-bg.js
@@ -1,10 +1,6 @@
 /*
-  Original code: https://codepen.io/supah/pen/ExabJxB
-  Made by: Fabio Ottaviani
-    Codepen Profile: https://codepen.io/supah/
-    X Profile: https://twitter.com/supahfunk
-    Instagram Profile: https://www.instagram.com/supahfunk/
-    Dribbble Profile: https://dribbble.com/supahfunk
+  Made by: Jonas Badalic
+  https://codepen.io/JonasBadalic
 */
 /*
   Modified by: JustDeveloper
@@ -12,158 +8,107 @@
 */
 
 
-/*--------------------
-Vars
---------------------*/
-const deg = (a) => Math.PI / 180 * a
-const rand = (v1, v2) => Math.floor(v1 + Math.random() * (v2 - v1))
-const opt = {
-  particles: window.width / 500 ? 1000 : 500,
-  noiseScale: 0.009,
-  angle: Math.PI / 180 * -90,
-  h1: rand(0, 360),
-  h2: rand(0, 360),
-  s1: rand(20, 90),
-  s2: rand(20, 90),
-  l1: rand(30, 80),
-  l2: rand(30, 80),
-  strokeWeight: 1.2,
-  tail: 82,
-}
-const Particles = []
-let time = 0/*
-document.body.addEventListener('click', () => {
-  opt.h1 = rand(0, 360)
-  opt.h2 = rand(0, 360)
-  opt.s1 = rand(20, 90)
-  opt.s2 = rand(20, 90)
-  opt.l1 = rand(30, 80)
-  opt.l2 = rand(30, 80)
-  opt.angle += deg(random(60, 60)) * (Math.random() > .5 ? 1 : -1)
-  
-  for (let p of Particles) {
-    p.randomize()
-  }
-})
-*/
 
-/*--------------------
-Particle
---------------------*/
-class Particle {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
-    this.lx = x
-    this.ly = y
-    this.vx = 0
-    this.vy = 0
-    this.ax = 0
-    this.ay = 0
-    this.hueSemen = Math.random()
-    this.hue = this.hueSemen > .5 ? 20 + opt.h1 : 20 + opt.h2
-    this.sat = this.hueSemen > .5 ? opt.s1 : opt.s2
-    this.light = this.hueSemen > .5 ? opt.l1 : opt.l2
-    this.maxSpeed = this.hueSemen > .5 ? 3 : 2
-  }
-  
-  randomize() {
-    this.hueSemen = Math.random()
-    this.hue = this.hueSemen > .5 ? 20 + opt.h1 : 20 + opt.h2
-    this.sat = this.hueSemen > .5 ? opt.s1 : opt.s2
-    this.light = this.hueSemen > .5 ? opt.l1 : opt.l2
-    this.maxSpeed = this.hueSemen > .5 ? 3 : 2
-  }
-  
-  update() {
-    this.follow()
-    
-    this.vx += this.ax
-    this.vy += this.ay
-    
-    var p = Math.sqrt(this.vx * this.vx + this.vy * this.vy)
-    var a = Math.atan2(this.vy, this.vx)
-    var m = Math.min(this.maxSpeed, p)
-    this.vx = Math.cos(a) * m
-    this.vy = Math.sin(a) * m
-    
-    this.x += this.vx
-    this.y += this.vy
-    this.ax = 0
-    this.ay = 0
-    
-    this.edges()
-  }
-  
-  follow() {
-    let angle = (noise(this.x * opt.noiseScale, this.y * opt.noiseScale, time * opt.noiseScale)) * Math.PI * 0.5 + opt.angle
-    
-    this.ax += Math.cos(angle)
-    this.ay += Math.sin(angle)
-    
-  }
-  
-  updatePrev() {
-    this.lx = this.x
-    this.ly = this.y
-  }
-  
-  edges() {
-    if (this.x < 0) {
-      this.x = width
-      this.updatePrev()
-    }
-    if (this.x > width) {
-      this.x = 0
-      this.updatePrev()
-    }
-    if (this.y < 0) {
-      this.y = height
-      this.updatePrev()
-    }
-    if (this.y > height) {
-      this.y = 0
-      this.updatePrev()
-    }
-  }
-  
-  render () {
-    stroke(`hsla(${this.hue}, ${this.sat}%, ${this.light}%, .5)`)
-    line(this.x, this.y, this.lx, this.ly)
-    this.updatePrev()
+var canvas = document.getElementById('container');
+var ctx = canvas.getContext('2d');
+var w = $('#blurCanvasTop').width();
+var h = $('#blurCanvasTop').height();
+var ww = $(window).width();
+var wh = $(window).height();
+canvas.width = ww;
+canvas.height= wh;
+var partCount = 100;
+var particles = [];
+
+function particle(){
+  this.color = 'rgba(255,255,255,'+ Math.random()+')';
+  console.log(this.color);
+  this.x = randomInt(0,ww);
+  this.y = randomInt(0,wh);
+  this.direction = {
+    "x": -1 + Math.random() * 2,
+    "y": -1 + Math.random() * 2
+  };
+  this.vx = 0.3 * Math.random();
+  this.vy = 0.3 * Math.random();
+  this.radius = randomInt(2,3);
+  this.float = function(){
+    this.x += this.vx * this.direction.x;
+    this.y += this.vy * this.direction.y;
+  };
+  this.changeDirection = function (axis) {
+    this.direction[axis] *= -1;
+  };
+  this.boundaryCheck = function () {
+            if (this.x >= ww) {
+                this.x = ww;
+                this.changeDirection("x");
+            } else if (this.x <= 0) {
+                this.x = 0;
+                this.changeDirection("x");
+            }
+            if (this.y >= wh) {
+                this.y = wh;
+                this.changeDirection("y");
+            } else if (this.y <= 0) {
+                this.y = 0;
+                this.changeDirection("y");
+            }
+        };
+  this.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fill();
+  };
+}
+function clearCanvas() {
+ ctx.clearRect(0, 0, ww, wh);
+}
+function createParticles(){
+  for (i=0;i<partCount;i++){
+    var p = new particle();
+    particles.push(p);
   }
 }
-
-
-/*--------------------
-Setup
---------------------*/
-function setup() {
-  createCanvas(windowWidth, windowHeight)
-  for (let i = 0; i < opt.particles; i++) {
-    Particles.push(new Particle(Math.random() * width, Math.random() * height))
-  }
-  strokeWeight(opt.strokeWeight)
+function drawParticles() {
+   for (i=0;i<particles.length;i++) {
+     p = particles[i];
+     p.draw();
+   }
 }
-
-
-/*--------------------
-Draw
---------------------*/
-function draw() {
-  time++
-  background(0, 100 - opt.tail)
-  
-  for (let p of Particles) {
-    p.update()
-    p.render()
-  }
+function updateParticles() {
+        for (var i = particles.length - 1; i >= 0; i--) {
+            p = particles[i];
+            p.float();
+            p.boundaryCheck();
+        }
 }
+createParticles();
+drawParticles();
+function animateParticles() {
+        clearCanvas();
+        drawParticles();
+        updateParticles();
+        requestAnimationFrame(animateParticles);
+    }
+requestAnimationFrame(animateParticles);
 
-
-/*--------------------
-Resize
---------------------*/
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
+$(window).on('resize',function(){
+  ww = $(window).width();
+  wh = $(window).height();
+  canvas.width = ww;
+  canvas.height= wh;
+  clearCanvas();
+  particles = [];
+  createParticles();
+  drawParticles();
+});
+function randomInt(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+function velocityInt(min,max)
+{
+    return Math.random()*(max-min+1)+min;
 }
