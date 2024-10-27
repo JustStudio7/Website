9f744734fee0a95380021c3b8dc38360a408b74a
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-10-27 14:35:20 +0500

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,30 @@
+
+/*
+
+MIT License
+
+Copyright (c) 2024 JustStudio. <https://juststudio.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 const headerElement = document.querySelector('.header');
 headerElement.innerHTML = `
 <div class="dropdown h-terms">
@@ -39,7 +66,7 @@ headerElement.innerHTML = `
 `;
 headerElement.parentElement.innerHTML += `
     <span class="home-footer1 footer">
-        <a href="https://github.com/JustStudio7/Website/blob/main/LICENSE">
+        <a href="https://github.com/JustStudio7/Website/blob/main/LICENSE" target="_blank">
               <span style="opacity: 0.5;text-decoration: underline;text-decoration-color: rgba(255,255,255,0.33);">© JustStudio. 2024</span>
         </a>
         <br>
