465ac9183c71829f19c976f23b59fc1fb9cf08d1
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-11-21 21:25:57 +0500

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,22 @@
+const splineViewer = document.querySelector('spline-viewer');
+let lastTime = performance.now();
+let frameCount = 0;
+
+function checkFPS() {
+  const currentTime = performance.now();
+  frameCount++;
+
+  if (currentTime - lastTime >= 1000) {
+      const fps = frameCount;
+      frameCount = 0;
+      lastTime = currentTime;
+
+      if (fps < 50) {
+          splineViewer.remove();
+      }
+  }
+
+  requestAnimationFrame(checkFPS);
+}
+
+requestAnimationFrame(checkFPS);
