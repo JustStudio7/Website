974bd4e62600d5ca40906f4ac777b330c102ce8e
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-12-18 20:09:56 +0300

    Update glow.js

--- js/glow.js
+++ js/glow.js
@@ -1,5 +1,3 @@
-console.clear();
-
 const cardsContainer = document.querySelector(".cards");
 const cardsContainerInner = document.querySelector(".cards__inner");
 const cards = Array.from(document.querySelectorAll(".card"));
