898c419d6b35433ba907530c840a233ceb463e6b
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-12-25 16:58:59 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,30 @@
-const splineViewer = document.querySelector('spline-viewer');
+/*
+
+MIT License
+
+Copyright (c) 2024 JustStudio. <https://juststudio.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+const spl_v = document.querySelector('spline-viewer');
 let lastTime = performance.now();
 let frameCount = 0;
 
@@ -12,7 +38,7 @@ function checkFPS() {
       lastTime = currentTime;
 
       if (fps < 35) {
-          splineViewer.remove();
+          spl_v.remove();
       }
   }
 
