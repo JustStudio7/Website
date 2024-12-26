aa6268510149f557a931f559019ddfda8614473e
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-12-26 18:16:03 +0300

    Create en.js

---
+++ js/lang/en.js
@@ -0,0 +1,74 @@
+/*
+
+MIT License
+
+Copyright (c) 2024 JustStudio. <https://juststudio.is-a.dev/>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+const Translations = {
+    "EN": {
+        "1": "JustStudio.",
+        "2": "a System Development Studio",
+        "3": "Terms",
+        "4": "User Agreement",
+        "5": "Rules For Players",
+        "6": "Products",
+        "7": "JustStudio.Accounts",
+        "8": "API",
+        "9": "Games",
+        "10": "Coming soon...",
+        "11": "EN",
+        "12": "© 2024 JustStudio.",
+        "13": "Rules for Players",
+        "14": "Rules for players playing games by \"JustStudio.\" on the Roblox platform.",
+        "15": "General",
+        "16": "Follow the <a target=\"_blank\" href=\"https://en.help.roblox.com/hc/en-us/articles/115004647846-Roblox-Terms-of-Use\" rel=\"noreferrer noopener nofollow\" style=\"text-decoration: underline;\">Terms of Use of the Roblox platform</a>. In short, a few simple rules: Do not swear. Treat everyone with respect. No dating, or other type of adult content. No discrimination. No propaganda of anything. It is also forbidden to mention/recreate terrorist attacks or something like that. Do not use cheats/exploits/anything like that. Play the games honestly, and enjoy the games. Note: The rules provided above are not the official rules of the Roblox platform. They may also be incorrect or outdated. Please read the <a target=\"_blank\" href=\"https://en.help.roblox.com/hc/en-us/articles/115004647846-Roblox-Terms-of-Use\" rel=\"noreferrer noopener nofollow\" style=\"text-decoration: underline;\">Roblox Terms of Use</a>.",
+        "17": "JustStudio.Rules",
+        "18": "Playing our games published on the Roblox platform means that you agree to these rules, as well as the User Agreement. If the violator does not know these rules or does not agree with these rules, this does not exempt him from punishment (temporary ban or ban forever). We have done everything to make sure that all players know these rules: We have placed links to our discord server wherever possible in Roblox, and through our discord server you can get to these rules. We are not partners/part/division/etc. of Roblox Corporation. All players must respect moderation and administration. If the moderation/administration has done some actions, as it seems to you, the actions were done by mistake or by accident, contact our support located on our <a href=\"https://discord.gg/aHXxS6VvcC\" target=\"_blank\"><span style=\"text-decoration: underline;\">Discord server</span></a>. Treat \"JustStudio.\", Administration, Moderation, Developers, and our Partners, and also other players with respect. Any kind of disrespect, bullying, etc. is strictly prohibited!",
+        "19": "About Bans",
+        "20": "About Bans on games and any other assets that created by \"JustStudio.\" on the Roblox platform. We reserve the right to prohibit banned users from playing games using our assets.",
+        "21": "Reason",
+        "22": "We can ban you on our games and services and assets if you violate the Rules for Players or the User Agreement. You can also get banned if: you are a friend/ally/partner/etc. of the one who got banned; or if you did an attack/raid on \"JustStudio.\" or were a participant of the attack/raid; or if you are trying to find loopholes in the rules or agreements or terms.",
+        "23": "Details",
+        "24": "If you are banned for actions in our game, then you will get temporary ban (depending on actions) in the game or a ban forever on services and assets created by \"JustStudio.\"; Or if you are banned for actions in any of our services/assets but not in our game, then you will get banned forever absolutely on all games/services/assets created by \JustStudio.\"",
+        "25": "Appeals",
+        "26": "If you think that you were banned by mistake, you can appeal on our <a href=\"https://discord.gg/aHXxS6VvcC\" target=\"_blank\"><span style=\"text-decoration: underline;\">Discord server</span></a>.",
+        "27": "Information",
+        "28": "Last Updated: December 25th, 2024<br>Version: 1.3.1",
+        "29": "User Agreement",
+        "30": "Visiting and using our website, as well as games and any of our other products, projects, etc., means that you agree to this user agreement, as well as all the rules and other terms and conditions.",
+        "31": "Terms of Use",
+        "32": "We may change these terms and all other terms and conditions without notifying anyone. Your continued use of the site, games, products, projects, etc. created by \"JustStudio.\", after changes to any terms or conditions or conditions, means your agreement to the changes. \"JustStudio.\" is a studio that develops mainly assets on the Roblox platform, but has projects not related to Roblox. Our websites and projects and products, etc., are located only on these domains: juststudio.is-a.dev, just.is-a.dev, j.is-a.dev, <a href=\"#domains\" class=\"stfl\">see the full list</a> (games published on the Roblox platform do not relate to this in any way), all other domains do not belong to us, except for team domains (<a href=\"#tdomains\" class=\"stfl\">see the full list</a>) and partner domains (reavercommunity.dcms.site, kappy.is-a.dev, <a href=\"#pdomains\" class=\"stfl\">see the full list</a>). Partner domains, websites, as well as team domains and websites are supported by our servers and services, but we have no relation to, and are not responsible for, the content on partner websites and domains, as well as team domains and websites. \"JustStudio.\" is in no way an affiliate or partner of Roblox Corporation, Microsoft Corporation, Google LLC, Discord Inc., Clerk Inc., Spline Inc. and GitHub Inc. We are not authorized, endorsed or sponsored by Roblox Corporation, Microsoft Corporation, Google LLC, Discord Inc., Clerk Inc., Spline Inc. and GitHub Inc. All Roblox trademarks remain the property of Roblox Corporation. All Microsoft trademarks remain the property of Microsoft Corporation. All Discord trademarks remain the property of Discord Inc. All GitHub trademarks remain the property of GitHub Inc. All Google trademarks remain the property of Google LLC. All Clerk trademarks remain the property of Clerk Inc. All Spline trademarks remain the property of Spline Inc.",
+        "33": "Confidentiality and Security",
+        "34": "We reserve the right to collect and store data about your account on the Roblox platform every time you visit our games or use our assets on the Roblox platform. We collect and store the following data about your account on the Roblox platform: UserID, UserName, UserDisplayName, Your role in our Roblox gruoup, AccountAge, <a class=\"stfl\" href=\"#data\">see the full list</a>, and your actions in our games. All the data about your account that we collect and store is provided by the Roblox platform, without using third-party resources. You can request the deletion of all your data on our servers and services on our <a href=\"https://discord.gg/aHXxS6VvcC\" target=\"_blank\"><span style=\"text-decoration: underline;\">Discord server</span></a>. Keep in mind that after deleting your data, if you will use our assets again or visit our games again, we will collect and save your data again. No third parties receive your data. Data about you is collected and stored to simplify bug fixes, as well as to find users who violate our agreements or rules. Also, some data is collected and stored to personalize something (for example, interface settings in \"JustStudio.AdminPanel\").",
+        "35": "Purchase of paid content",
+        "36": "Purchasing any paid content means that you agree to these terms. The purchase of paid content is not required, the purchase is made according to your desire, we do not force anyone to buy anything. After purchase, the paid content in the games is given to you automatically, no errors can occur. Paid roles in our <a href=\"https://discord.gg/aHXxS6VvcC\" target=\"_blank\"><span style=\"text-decoration: underline;\">Discord server</span></a> are given to you manually, by the head administration. Funds for paid content will not be refunded.",
+        "37": "Information",
+        "38": "Last Updated: December 26th, 2024<br>Version: 1.0.4",
+        "39": "next",
+        "40": "back",
+        "41": "Domains owned by \"JustStudio.\"",
+        "42": "Team domains owned by \"JustStudio.\"",
+        "43": "Partner domains owned by \"JustStudio.\"",
+        "44": "Data we collect about your Roblox account"
+    }
+}
