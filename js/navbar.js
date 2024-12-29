93eadf49965e198741f9711a55727498580b0e7b
JustDeveloper <justdeveloper@juststudio.is-a.dev>
2024-12-29 13:36:37 +0300

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -343,28 +343,6 @@ if (!headerState) {
     }
 }
 
-let connectionSVGCached = true
-try {
-    fetch('https://juststudio.is-a.dev/img/connection.svg')
-        .then(response => {
-            if (response.ok && response.headers.get('Content-Type') === 'image/svg+xml') {
-                return response.blob();
-            } else {
-                throw new Error('Not a valid SVG image or response error');
-            }
-        })
-        .then(blob => {
-            caches.open('svg-cache').then(cache => {
-                cache.put(new Request(url), new Response(blob));
-            });
-        })
-        .catch(error => {
-            connectionSVGCached = false;
-        });
-} catch {
-    connectionSVGCached = false;
-}
-
 let terms_accepted = localStorage.getItem('d1');
 function notify(icon, text, buttonText, id, blur, type) {
     "use strict";
@@ -387,7 +365,9 @@ function notify(icon, text, buttonText, id, blur, type) {
                 notificsElement.innerHTML = `<span class="notification">${text}<button id="${id}">${buttonText}</button></span>`;
             }
         } else { 
-            if (icon) {
+            if (type == 'wifi') {
+                notificsElement.innerHTML = `<span class="notification"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1437f3"><path d="M762-84 414-434q-31 7-59.5 19T301-386q-21 14-46.5 14.5T212-389q-18-18-16.5-43.5T217-473q23-17 48.5-31t52.5-26l-90-90q-26 14-50.5 29.5T130-557q-20 16-45.5 16T42-559q-18-18-17-43t21-41q22-18 45-34.5t49-30.5l-56-56q-11-11-11-28t11-28q11-11 28-11t28 11l679 679q12 12 12 28.5T819-84q-12 11-28.5 11.5T762-84Zm-282-36q-42 0-71-29.5T380-220q0-42 29-71t71-29q42 0 71 29t29 71q0 41-29 70.5T480-120Zm273-275q-16 16-37.5 15.5T678-396l-10-10-10-10-96-96q-13-13-5-27t28-9q45 11 85.5 31t75.5 47q18 14 20.5 36.5T753-395Zm165-164q-17 18-42 18.5T831-556q-72-59-161.5-91.5T480-680q-21 0-40.5 1.5T400-674q-25 4-45-10.5T331-724q-4-25 11-45t40-24q24-4 48.5-5.5T480-800q125 0 235.5 41.5T914-644q20 17 21 42t-17 43Z"/></svg>${text}</span>`;
+            } else if (icon) {
                 notificsElement.innerHTML = `<span class="notification"><img href="${icon}"></img>${text}</span>`;
             } else {
                 notificsElement.innerHTML = `<span class="notification">${text}</span>`;
@@ -451,12 +431,9 @@ function checkUserAgreement() {
     }
 }
 checkUserAgreement()
+let n_connection;
 window.addEventListener('offline', function() {
-    if (connectionSVGCached) {
-        let n_connection = notify('img/connection.svg', 'No Internet connection!', null, null, true);
-    } else {
-        let n_connection = notify(null, 'No Internet connection!', null, null, true);
-    }
+    n_connection = notify(null, 'No Internet connection!', null, null, true, 'wifi');
 });
 window.addEventListener('online', function() {
     r_notific(n_connection, true);
