ba82a809b8a716557afad6ecc823b973e6c87740
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-04-22 20:51:22 +0500

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -24,7 +24,6 @@ SOFTWARE.
 
 */
 
-const slashslash = atob('Ly8=');
 const headerElement = document.querySelector('.header');
 if (headerElement) {
     headerElement.innerHTML = `
@@ -32,8 +31,8 @@ if (headerElement) {
         <div class="trigger">
             <span class="txt3">Terms</span>
             <ul class="buttons">
-                <li><a href="https:${slashslash}juststudio.is-a.dev/terms" class="txt4">User Agreement</a></li>
-                <li><a href="https:${slashslash}juststudio.is-a.dev/rules" class="txt5">Rules For Players</a></li>
+                <li><a href="https://juststudio.is-a.dev/terms" class="txt4">User Agreement</a></li>
+                <li><a href="https://juststudio.is-a.dev/rules" class="txt5">Rules For Players</a></li>
             </ul>
         </div>
     </div>
@@ -42,7 +41,7 @@ if (headerElement) {
         <div class="trigger">
             <span class="txt6">Products</span>
             <ul class="buttons">
-                <li><a href="https:${slashslash}encoder.js.is-a.dev/" class="txt7">Encoder.js</a></li>
+                <li><a href="https://encoder.js.is-a.dev/" class="txt7">Encoder.js</a></li>
             </ul>
         </div>
     </div><div class="dropdown h-games">
@@ -56,30 +55,30 @@ if (headerElement) {
     </div>
     <div class="h-main">
         <span class="txt1">JustStudio.</span>
-        <a href="https:${slashslash}discord.gg/aHXxS6VvcC" target="_blank" rel="noreferrer noopener">
-            <img src="https:${slashslash}juststudio.is-a.dev/img/discord.white.svg" alt="Discord Server">
+        <a href="https://discord.gg/aHXxS6VvcC" target="_blank" rel="noreferrer noopener">
+            <img src="https://juststudio.is-a.dev/img/discord.white.svg" alt="Discord Server">
         </a>
-        <a target="_blank" rel="noreferrer noopener" href="https:${slashslash}www.roblox.com/groups/11641563/JustStudio#!/about">
-            <img src="https:${slashslash}juststudio.is-a.dev/img/roblox.white.svg" alt="Roblox Community">
+        <a target="_blank" rel="noreferrer noopener" href="https://www.roblox.com/groups/11641563/JustStudio#!/about">
+            <img src="https://juststudio.is-a.dev/img/roblox.white.svg" alt="Roblox Community">
         </a>
-        <a target="_blank" rel="noreferrer noopener" href="https:${slashslash}github.com/JustStudio7">
-            <img src="https:${slashslash}juststudio.is-a.dev/img/github.white.svg" alt="GitHub Profile">
+        <a target="_blank" rel="noreferrer noopener" href="https://github.com/JustStudio7">
+            <img src="https://juststudio.is-a.dev/img/github.white.svg" alt="GitHub Profile">
         </a>
         <a target="_blank" rel="noreferrer noopener" id="header-switch">
-            <img src="https:${slashslash}juststudio.is-a.dev/img/arrow.svg" alt="Header Style Switch" style="margin-left: 5px; height: 24px; width: 24px;">
+            <img src="https://juststudio.is-a.dev/img/arrow.svg" alt="Header Style Switch" style="margin-left: 5px; height: 24px; width: 24px;">
         </a>
-        <a target="_blank" rel="noreferrer noopener" href="https:${slashslash}juststudio.is-a.dev/#team" style="display:none">
+        <a target="_blank" rel="noreferrer noopener" href="https://juststudio.is-a.dev/#team" style="display:none">
             <span>team</span>
         </a>
     </div>
         <div id="acc-btn" style="display:none;">
-            <svg xmlns="http:${slashslash}www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M720-520h-80q-17 0-28.5-11.5T600-560q0-17 11.5-28.5T640-600h80v-80q0-17 11.5-28.5T760-720q17 0 28.5 11.5T800-680v80h80q17 0 28.5 11.5T920-560q0 17-11.5 28.5T880-520h-80v80q0 17-11.5 28.5T760-400q-17 0-28.5-11.5T720-440v-80Zm-360 40q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-240v-32q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v32q0 33-23.5 56.5T600-160H120q-33 0-56.5-23.5T40-240Zm80 0h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M720-520h-80q-17 0-28.5-11.5T600-560q0-17 11.5-28.5T640-600h80v-80q0-17 11.5-28.5T760-720q17 0 28.5 11.5T800-680v80h80q17 0 28.5 11.5T920-560q0 17-11.5 28.5T880-520h-80v80q0 17-11.5 28.5T760-400q-17 0-28.5-11.5T720-440v-80Zm-360 40q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM40-240v-32q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v32q0 33-23.5 56.5T600-160H120q-33 0-56.5-23.5T40-240Zm80 0h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0-80Zm0 400Z"></path></svg>
         </div>
         <div id="theme-switch">
-            <svg xmlns="http:${slashslash}www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1437f3">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1437f3">
                 <path d="M480-120q-151 0-255.5-104.5T120-480q0-138 90-239.5T440-838q13-2 23 3.5t16 14.5q6 9 6.5 21t-7.5 23q-17 26-25.5 55t-8.5 61q0 90 63 153t153 63q31 0 61.5-9t54.5-25q11-7 22.5-6.5T819-479q10 5 15.5 15t3.5 24q-14 138-117.5 229T480-120Z"></path>
             </svg>
-            <svg xmlns="http:${slashslash}www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#6e3bf3">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#6e3bf3">
                 <path d="M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM80-440q-17 0-28.5-11.5T40-480q0-17 11.5-28.5T80-520h80q17 0 28.5 11.5T200-480q0 17-11.5 28.5T160-440H80Zm720 0q-17 0-28.5-11.5T760-480q0-17 11.5-28.5T800-520h80q17 0 28.5 11.5T920-480q0 17-11.5 28.5T880-440h-80ZM480-760q-17 0-28.5-11.5T440-800v-80q0-17 11.5-28.5T480-920q17 0 28.5 11.5T520-880v80q0 17-11.5 28.5T480-760Zm0 720q-17 0-28.5-11.5T440-80v-80q0-17 11.5-28.5T480-200q17 0 28.5 11.5T520-160v80q0 17-11.5 28.5T480-40ZM226-678l-43-42q-12-11-11.5-28t11.5-29q12-12 29-12t28 12l42 43q11 12 11 28t-11 28q-11 12-27.5 11.5T226-678Zm494 495-42-43q-11-12-11-28.5t11-27.5q11-12 27.5-11.5T734-282l43 42q12 11 11.5 28T777-183q-12 12-29 12t-28-12Zm-42-495q-12-11-11.5-27.5T678-734l42-43q11-12 28-11.5t29 11.5q12 12 12 29t-12 28l-43 42q-12 11-28 11t-28-11ZM183-183q-12-12-12-29t12-28l43-42q12-11 28.5-11t27.5 11q12 11 11.5 27.5T282-226l-42 43q-11 12-28 11.5T183-183Z"></path>
             </svg>
         </div>
@@ -91,7 +90,7 @@ if (headerElement) {
 const currentYear = new Date().getFullYear();
 const copyrightText = `
     <span class="home-footer1 footer">
-        <a href="https:${slashslash}github.com/JustStudio7/Website/blob/main/LICENSE" target="_blank">
+        <a href="https://github.com/JustStudio7/Website/blob/main/LICENSE" target="_blank">
             <span style="opacity: 0.5;text-decoration: underline;text-decoration-color: rgba(255,255,255,0.33);" class="txt12" id="copyrightfootertext">© 2024-${currentYear} JustStudio.</span>
         </a>
         <br>
@@ -137,7 +136,7 @@ themeSwitch.addEventListener("dblclick", () => {
 });
 
 acc_btn.addEventListener("click", () => {
-  window.location.href = `https:${slashslash}juststudio.is-a.dev/account/`;
+  window.location.href = `https://juststudio.is-a.dev/account/`;
 });
 
 if (!theme) {
@@ -155,13 +154,13 @@ if (!theme) {
     if (theme !== "special" && theme !== "dark" && theme !== "light") {
       let ERROR_NAME = 'Unknown theme';
       let ERROR_ID = 1;
-      fetch(`https:${slashslash}juststudio.is-a.dev/data/visual-error.txt`)
+      fetch(`https://juststudio.is-a.dev/data/visual-error.txt`)
         .then(response => response.text())
         .then(data => {
           const lines = data.split('\n').slice(29).join('\n');
           document.body.innerHTML += lines;
         });
-      fetch(`https:${slashslash}juststudio.is-a.dev/js/error-handler.js`)
+      fetch(`https://juststudio.is-a.dev/js/error-handler.js`)
         .then(response => response.text())
         .then(data => {
             eval(data);
@@ -186,7 +185,7 @@ function initializeMultiLanguageSupportSystem() {
         const currentYear = new Date().getFullYear();
         
         function fetchTranslations(newLang) {
-            const url = newLang === 'RU' ? `https:${slashslash}juststudio.is-a.dev/js/lang/ru.js` : `https:${slashslash}juststudio.is-a.dev/js/lang/en.js`;
+            const url = newLang === 'RU' ? `https://juststudio.is-a.dev/js/lang/ru.js` : `https://juststudio.is-a.dev/js/lang/en.js`;
             return fetch(url)
                 .then(response => response.text())
                 .then(data => {
@@ -283,13 +282,13 @@ function initializeMultiLanguageSupportSystem() {
                     (function() {
                       let ERROR_NAME = 'Unknown language';
                       let ERROR_ID = 2;
-                      fetch(`https:${slashslash}juststudio.is-a.dev/data/visual-error.txt`)
+                      fetch(`https://juststudio.is-a.dev/data/visual-error.txt`)
                         .then(response => response.text())
                         .then(data => {
                           const lines = data.split('\n').slice(29).join('\n');
                           document.body.innerHTML += lines;
                         });
-                      fetch(`https:${slashslash}juststudio.is-a.dev/js/error-handler.js`)
+                      fetch(`https://juststudio.is-a.dev/js/error-handler.js`)
                         .then(response => response.text())
                         .then(data => {
                             eval(data);
@@ -359,13 +358,13 @@ if (!headerState) {
     if (headerState !== "alternative" && headerState !== "default") {
       let ERROR_NAME = 'Unknown header state';
       let ERROR_ID = 3;
-      fetch(`https:${slashslash}juststudio.is-a.dev/data/visual-error.txt`)
+      fetch(`https://juststudio.is-a.dev/data/visual-error.txt`)
         .then(response => response.text())
         .then(data => {
           const lines = data.split('\n').slice(29).join('\n');
           document.body.innerHTML += lines;
         });
-      fetch(`https:${slashslash}juststudio.is-a.dev/js/error-handler.js`)
+      fetch(`https://juststudio.is-a.dev/js/error-handler.js`)
         .then(response => response.text())
         .then(data => {
             eval(data);
@@ -374,7 +373,7 @@ if (!headerState) {
 };
 
 let terms_accepted = localStorage.getItem('d1');
-const svgxmlns = `http:${slashslash}www.w3.org/2000/svg`;
+const svgxmlns = `http://www.w3.org/2000/svg`;
 function notify(icon, text, buttonText, id, blur, type) {
     "use strict";
     let notificsElement = globalThis.document.getElementById('notifications');
